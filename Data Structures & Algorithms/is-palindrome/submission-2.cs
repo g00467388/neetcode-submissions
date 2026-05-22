902cf9e6c12706c5607e630289766c5ab31f4808
Tomás Hogan <g00467388@atu.ie>
2026-05-22 17:57:36 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    
+      public bool IsPalindrome(string s)
+    {
+        string palindrome;
+
+        StringBuilder sb = new(); 
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsLetterOrDigit(s[i]))
+                sb.Append(char.ToLower(s[i] ));
+        }
+        palindrome = sb.ToString();
+
+        int endPtr = palindrome.Length -1; 
+        for (int i = 0; i < palindrome.Length; i++)
+        {
+            if (palindrome[i] != palindrome[endPtr])
+                return false;
+            endPtr -= 1; 
+            Console.WriteLine($"{nameof(endPtr)}: {endPtr}, {nameof(i)}: {i}");
+        }
+        return true; 
+    }
+
+
+}
