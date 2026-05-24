37974e4e61cd624e94f03f35449daacafa205aa3
Tomás Hogan <g00467388@atu.ie>
2026-05-24 19:15:44 +0100

    Add: linked-list-cycle-detection - submission-21

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-21.cs
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+ 
+ 
+    public bool HasCycle(ListNode head)
+    {
+        if (head is null)
+            return false;
+        ListNode slow = head; 
+        ListNode fast = head; 
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next; 
+            fast = fast.next.next;
+            if (slow == fast)
+                return true;
+        }
+        return false;
+    }
+
+}
