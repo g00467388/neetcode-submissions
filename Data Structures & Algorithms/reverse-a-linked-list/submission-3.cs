be1f8dbacc20f6eaadc3c9f63ce55cf23e09a0db
Tomás Hogan <g00467388@atu.ie>
2026-07-12 20:37:33 +0100

    Add: reverse-a-linked-list - submission-3

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-3.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+   
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null)
+        {
+            var tmp = current.next;
+            current.next = prev;
+            prev = current;
+            current = tmp;
+        }
+
+        return prev; 
+    }
+}
