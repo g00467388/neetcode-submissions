05261ce22ca708b646d7f1d97e361fc0ae900e65
Tomás Hogan <g00467388@atu.ie>
2026-07-13 19:51:16 +0100

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public string Encode(IList<string> strs)
+    {
+        string result = string.Empty;
+        for (int i = 0; i < strs.Count; i++)
+        {
+            result += $"{strs[i]}´";
+        }
+
+        return result;
+    }
+
+    public List<string> Decode(string s)
+    {
+        List<string> result = new();
+            string value = string.Empty;
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            if (s[i] == '´')
+            {
+                result.Add(value);
+                value = string.Empty;
+            }
+            else
+                value += s[i];
+            
+        }
+
+        return result;
+    }
+
+}
